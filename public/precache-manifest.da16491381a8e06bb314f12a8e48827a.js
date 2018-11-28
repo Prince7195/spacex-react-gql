d0e42bba16cc583f77352f8e779e3b276bdf5cac
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-11-29 01:13:09 +0530

    react gql app

---
+++ public/precache-manifest.da16491381a8e06bb314f12a8e48827a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4dfff6af7fab8641ffcf",
+    "url": "/static/js/main.4dfff6af.chunk.js"
+  },
+  {
+    "revision": "992b7edd05fb59ff923d",
+    "url": "/static/js/1.992b7edd.chunk.js"
+  },
+  {
+    "revision": "4dfff6af7fab8641ffcf",
+    "url": "/static/css/main.654ddeb2.chunk.css"
+  },
+  {
+    "revision": "35e4d01884baace48e3a065283ce0e91",
+    "url": "/index.html"
+  }
+];
