53c77daea076a2f001c5b252a1bbb69fa08f2020
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-11-29 01:39:45 +0530

    added client files

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ApoloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter, Route } from "react-router-dom";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import "./App.css";
+import logo from "./logo.png";
+
+const client = new ApoloClient({
+  uri: "/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <div className="container">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: "block", margin: "auto" }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
